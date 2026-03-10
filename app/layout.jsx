fe86c782e7358ce53fe2bebd40d623e56850f894
hanynan <hanynan8@gmail.com>
2026-03-10 03:48:50 +0200

    layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { Rubik, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Navbar from './(components)/navbar';
+import Script from 'next/script';
 import Footer from './(components)/footer';
 
 const rubik = Rubik({
@@ -16,8 +17,6 @@ const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
 });
-
-
 
 
 
@@ -135,6 +134,7 @@ export const metadata = {
     ]
   }
 };
+const FB_PIXEL_ID = '891356743903601';
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ---------------------------
@@ -146,6 +146,34 @@ export default function RootLayout({ children }) {
     <head>
       <link rel="icon" href="/coding.png" />
       <meta name="facebook-domain-verification" content="d13bgz1ob2fbw3y3hv32ufn6o2p7ec" />
+
+        {/* --------------------------- */}
+        {/* Meta Pixel Script */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        
+        {/* Meta Pixel noscript fallback (for users with JavaScript disabled) */}
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
 
     </head>
       <body className={`${rubik.className} ${geistMono.variable} antialiased`}>
