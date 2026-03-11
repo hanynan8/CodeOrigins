08ca1fcf26152ddb5aea2a3e41158e17f31ac6c5
hanynan <hanynan8@gmail.com>
2026-03-11 19:15:17 +0200

    5

--- app/layout.jsx
+++ app/layout.jsx
@@ -134,8 +134,7 @@ export const metadata = {
     ]
   }
 };
-
-const FB_PIXEL_ID = '943138481395084';
+const FB_PIXEL_ID = '891356743903601';
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ---------------------------
@@ -148,9 +147,26 @@ export default function RootLayout({ children }) {
       <link rel="icon" href="/coding.png" />
       <meta name="facebook-domain-verification" content="d13bgz1ob2fbw3y3hv32ufn6o2p7ec" />
 
+        {/* --------------------------- */}
         {/* Meta Pixel Script */}
         <Script id="fb-pixel" strategy="afterInteractive">
           {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        
+<Script>
+          {`
+
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -159,11 +175,15 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '${FB_PIXEL_ID}');
+fbq('init', '891356743903601');
 fbq('track', 'PageView');
           `}
-        </Script>
-        
+
+</Script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=891356743903601&ev=PageView&noscript=1"
+/>
+</noscript>
         {/* Meta Pixel noscript fallback (for users with JavaScript disabled) */}
         <noscript>
           <img 
